91956bd7487d85b675cbd2bddf9e671ef90a7962
Dylan Faelker <84638978+dylanfaelker@users.noreply.github.com>
2022-05-16 20:52:38 -0400

    Added animation for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,16 @@
 const Project = ({ project, onHover, onClick}) => {
     return (
-        <div
-            className='project' 
-            onMouseEnter={() => onHover(project.id, false)} 
-            onMouseLeave={() => onHover(project.id, true)}
-            onClick={() => onClick(project.link)}
-        >
-            {project.display ? project.name : project.desc}
+        <div class="project" data-description={project.desc}>
+            {project.name}
         </div>
+    // <div
+        //     className='project' 
+        //     onMouseEnter={() => onHover(project.id, false)} 
+        //     onMouseLeave={() => onHover(project.id, true)}
+        //     onClick={() => onClick(project.link)}
+        // >
+        //     {project.display ? project.name : project.desc}
+        // </div>
     )
 }
 
