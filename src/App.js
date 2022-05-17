709b148009d5fba669c5eb55a84833f8eb554b3b
Dylan Faelker <84638978+dylanfaelker@users.noreply.github.com>
2022-05-16 20:51:29 -0400

    Added animation for projects

--- src/App.js
+++ src/App.js
@@ -22,50 +22,35 @@ function App() {
     {
       id: 1,
       name: <h3>Infinity Chess + Chess AI (Abbott)</h3>,
-      desc: <div>
-              <p className='paragraph'>A game which extends upon the traditional chess experience by adding a feature where the edges of the board wrap. Basically a combination of Pacman and Chess. I have also made an engine so it can be played on this website, however it is weak.</p>
-              <p className='paragraph'>Languages and Libraries: React.js</p>
-            </div>,
+      desc: "A game which extends upon the traditional chess experience by adding a feature where the edges of the board wrap. Basically a combination of Pacman and Chess. I have also made an engine so it can be played on this website, however it is weak. \n \n Languages and Libraries: React.js",
       display: true,
       link: 2,
     },
     {
       id: 2,
       name: <h3>Stock Porfolio Creater</h3>,
-      desc: <div>
-              <p className='paragraph'>Creates an investment portfolio of ten SP500 stocks that is considered risky. It uses data analytics with panda's and numpy to ensure a risky portfolio is chosen.</p>
-              <p className='paragraph'>Languages and Libraries: Python, Numpy, Pandas, YFinance</p>
-            </div>,
+      desc: "Creates an investment portfolio of ten SP500 stocks that is considered risky. It uses data analytics with panda's and numpy to ensure a risky portfolio is chosen. \n \n Languages and Libraries: Python, Numpy, Pandas, YFinance",
       display: true,
       link: 'https://github.com/dylpykill/StockPortfolioCreater',
     },
     {
       id: 3,
       name: <h3>Personal Website</h3>,
-      desc: <div>
-              <p className='paragraph'>What you are currently looking at.</p>
-              <p className='paragraph'>Languages and Libraries: React.js</p>
-            </div>,
+      desc: "What you are currently looking at. \n \n Languages and Libraries: React.js",
       display: true,
       link: 'https://github.com/dylpykill/PersonalWebsite'
     },
     {
       id: 4,
       name: <h3>FEN Decoder</h3>,
-      desc: <div>
-              <p className='paragraph'>FEN (Forsyth–Edwards Notation) is a chess notation used by computers to easily understand a position. To humans, it can be difficult to quickly recognize what is happening. This program parses the FEN chess notation into a visual chess board.</p>
-              <p className='paragraph'>Languages and Libraries: Java</p>
-            </div>,
+      desc: "FEN (Forsyth–Edwards Notation) is a chess notation used by computers to easily understand a position. To humans, it can be difficult to quickly recognize what is happening. This program parses the FEN chess notation into a visual chess board. \n \n Languages and Libraries: Java",
       display: true,
       link: 'https://github.com/dylpykill/FENDecoder'
     },
     {
       id: 5,
       name: <h3>Santa's Elf (Incomplete)</h3>,
-      desc: <div>
-              <p className='paragraph'>A discord bot that will organise secret Santa without the need for a human organiser making it quicker and easier to arrange.</p>
-              <p className='paragraph'>Languages and Libraries: Python, Discord Bot API</p>
-            </div>,
+      desc: "A discord bot that will organise secret Santa without the need for a human organiser making it quicker and easier to arrange. \n \n Languages and Libraries: Python, Discord Bot API",
       display: true,
       link: 'https://github.com/dylpykill/Santa-s-Elf'
     },
@@ -191,13 +176,13 @@ function App() {
       const position = window.pageYOffset;
       setScrollPosition(position);
 
-      const size = Math.max(0.1*window.innerHeight, 0.5*window.innerHeight - position);
+      const size = Math.max(0.01*window.innerHeight, 0.5*window.innerHeight - position);
       setNavbarSize(size);
   };
 
   const handleResize = () => {
     const position = window.pageYOffset;
-    const size = Math.max(0.1*window.innerHeight, 0.5*window.innerHeight - position);
+    const size = Math.max(0.01*window.innerHeight, 0.5*window.innerHeight - position);
     setNavbarSize(size);
   };
 
@@ -233,10 +218,10 @@ function App() {
   }
 
   return (
-    <div class="page" style={{height: '1100px'}}>
+    <div class="page">
       {(scrollPosition <= 0) ?
       <div>
-        <nav class="upperLanding">
+        <div class="upperLanding">
           <div class="contacts">
             <a
               href={ResumePDF}
@@ -275,8 +260,8 @@ function App() {
             </a>
           </div>
           <h1 class="name first">DYLAN</h1>
-        </nav>
-        <div>
+        </div>
+        <div class="lowerLanding">
           <h1 class="name last">FAELKER</h1>
           <DownArrow class="scrollArrow"/>
         </div>
@@ -326,6 +311,22 @@ function App() {
         <div>
           <h1 class="name last lastNameScroll">FAELKER</h1>
           <DownArrow class="scrollArrow disapear"/>
+        </div>
+        <div class="main">
+          <div class="section">
+            <img src={profile} className="profile" alt="logo"/>
+            <h2 >ABOUT ME</h2>
+            <p>I am currently taking my undergraduate degree at Waterloo for Computing and Financial Management (BCFM). The program is similar to a double major in Computer Science and Finance.</p>
+            <br></br>
+            <p>In high school, I really enjoyed analysing a company in grade 12 accounting and I liked the problem solving in computer science.</p>
+            <br></br>
+            <p>I am also inspired by the growth of fintech and look forawrd to opportunities to bring my knowledge of computing and financial management to use.</p>
+          </div>
+          <Projects 
+            projects={projects}
+            onHover={showDesc}
+            onClick={openInNewTab}
+          />
         </div>
       </div>}
     </div>
