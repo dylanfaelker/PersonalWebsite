f1a0702122b550427daf6a373bd38b27790d64ec
Dylan Faelker <84638978+dylanfaelker@users.noreply.github.com>
2022-05-21 01:49:51 -0400

    Updated contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,19 +1,48 @@
-import Contact from './Contact'
+import { ReactComponent as InstagramIcon } from '../instagram.svg'
+import { ReactComponent as GithubIcon } from '../github.svg'
+import { ReactComponent as EmailIcon } from '../email.svg'
+import { ReactComponent as LinkedinIcon } from '../linkedin.svg'
+import { ReactComponent as PageIcon } from '../page.svg'
+import ResumePDF from '../Resume.pdf'
 
-const Contacts = ({ contacts, onHover }) => {
+const Contacts = () => {
     return (
-        <div className='section'>
-            <header className='header'>
-                Contacting Me
-            </header>
-
-            {contacts.map((contact) => (
-                <Contact
-                    key={contact.id} 
-                    contact={contact}
-                    onHover={onHover}
-                />
-            ))}
+        <div class="contacts">
+            <a
+            href={ResumePDF}
+            target="_blank"
+            rel="noreferrer"
+            >
+            <PageIcon class="contact"/>
+            </a>
+            <a
+            href="mailto:faelkerd@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <EmailIcon class="contact"/>
+            </a>
+            <a
+            href="https://github.com/dylanfaelker"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <GithubIcon class="contact"/>
+            </a>
+            <a
+            href="https://www.linkedin.com/in/dylanfaelker/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <LinkedinIcon class="contact"/>
+            </a>
+            <a
+            href="https://www.instagram.com/d_faelker/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <InstagramIcon class="contact"/>
+            </a>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import { ReactComponent as InstagramIcon } from '../instagram.svg'
-import { ReactComponent as GithubIcon } from '../github.svg'
-import { ReactComponent as EmailIcon } from '../email.svg'
-import { ReactComponent as LinkedinIcon } from '../linkedin.svg'
-import { ReactComponent as PageIcon } from '../page.svg'
-import ResumePDF from '../Resume.pdf'
+import Contacts from './Contacts'
 import { Link } from "react-router-dom"
 
 const Navbar = ({ isToTop }) => {
@@ -22,58 +17,8 @@ const Navbar = ({ isToTop }) => {
                     <h4 onClick={goToTop}>DYLAN FAELKER</h4>
                 </Link>
             }
-            <div class="contacts">
-                <a
-                href={ResumePDF}
-                target="_blank"
-                rel="noreferrer"
-                >
-                <PageIcon class="contact"/>
-                </a>
-                <a
-                href="mailto:faelkerd@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <EmailIcon class="contact"/>
-                </a>
-                <a
-                href="https://github.com/dylanfaelker"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <GithubIcon class="contact"/>
-                </a>
-                <a
-                href="https://www.linkedin.com/in/dylanfaelker/"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <LinkedinIcon class="contact"/>
-                </a>
-                <a
-                href="https://www.instagram.com/d_faelker/?hl=en"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                <InstagramIcon class="contact"/>
-                </a>
-            </div>
+            <Contacts/>
         </nav>
-        // <nav className='navbar'>
-        //     <h2 className='nav-name'>Dylan Faelker</h2>
-        //     <div className='nav-choice'>
-        //         {navs.map((nav) => (
-        //             <Nav
-        //                 key={nav.id} 
-        //                 nav={nav}
-        //                 onHover={onHover}
-        //                 onClick={onClick}
-        //             />
-        //         ))}
-        //     </div>
-            
-        // </nav>
     )
 }
 
