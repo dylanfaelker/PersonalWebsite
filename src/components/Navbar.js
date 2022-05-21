63f5c005ae2e513aef3b0a1278dc33cb0b1bcc67
Dylan Faelker <84638978+dylanfaelker@users.noreply.github.com>
2022-05-21 12:05:50 -0400

    Move title of page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Contacts from './Contacts'
 import { Link } from "react-router-dom"
 
-const Navbar = ({ isToTop }) => {
+const Navbar = ({ isToTop, pageTitle }) => {
     const goToTop = () => {
         window.scrollTo({
             top: 0
@@ -13,10 +13,11 @@ const Navbar = ({ isToTop }) => {
         <nav class="topnav">
             {isToTop ?
                 <h4 class="smallName appear" onClick={goToTop}>DYLAN FAELKER</h4>:
-                <Link to="/" class="smallName appear">
+                <Link to="/" class="smallName">
                     <h4 onClick={goToTop}>DYLAN FAELKER</h4>
                 </Link>
             }
+            <h4 class="page-title">{pageTitle}</h4>
             <Contacts/>
         </nav>
     )
