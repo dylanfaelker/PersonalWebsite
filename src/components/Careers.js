b866c7e8243367dc45d45b8fa78dfcd63c4e1816
Dylan Faelker <84638978+dylanfaelker@users.noreply.github.com>
2022-05-21 17:28:18 -0400

    Added timeline component

---
+++ src/components/Careers.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Timeline from './Timeline'
+import ResumePDF from '../Resume.pdf'
+import { ReactComponent as PageIcon } from '../page.svg'
+
+const Careers = ({ careers }) => {
+    return (
+        <div className='section'>
+            <div class="flex-row">
+                <h2>CAREER</h2>
+                <a href={ResumePDF} target="_blank" rel="noreferrer">
+                    <PageIcon class="contact-large"/>
+                </a>
+            </div>
+
+            <Timeline points={careers}/>
+        </div>
+    )
+}
+
+export default Careers
