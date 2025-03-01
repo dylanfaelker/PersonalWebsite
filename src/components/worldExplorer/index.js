6d56bc6b09ac992c0b957b5e9af51ab8885de039
Dylan Faelker <faelkerd@gmail.com>
2025-03-01 22:51:57 +0000

    updating components index file

--- src/components/worldExplorer/index.js
+++ src/components/worldExplorer/index.js
@@ -1,4 +1,5 @@
 export { default as CoordInput } from "./CoordInput"
 export { default as EquirectangleMap } from "./EquirectangleMap"
 export { default as CoordInterface } from "./CoordInterface"
+export { default as CountryChecker } from "./CountryChecker"
 export * from "./mapCalcs"
