1781f9aacc7897a0475d452237991b9e68c8d6b6
Dylan Faelker <faelkerd@gmail.com>
2025-03-02 00:12:17 +0000

    MUI-ifying the layout of stocks page

--- src/pages/PortfolioGenerator.js
+++ src/pages/PortfolioGenerator.js
@@ -3,71 +3,80 @@ import { StockGraph } from '../components/stock-portfolio'
 import { useDispatch } from 'react-redux'
 import { setTitle } from '../redux/slice/globalSlice'
 import { SectionSubtitle, SectionTitle } from '../components/common'
+import { Box, List, ListItem, Typography, useTheme } from '@mui/material'
 
 function PortfolioGenerator() {
+
+  const theme = useTheme()
 
   const dispatch = useDispatch()
   useEffect(() => {dispatch(setTitle("PORTFOLIO GENERATOR"))})
 
   return (
-    <div class="page">
-      <div class='main'>
-        <div class="section">
-          <StockGraph/>
-        </div>
-        <div class="section">
-          <SectionTitle>DEMANDS</SectionTitle>
-          <ul>
-            <li>Must always have 10 Stocks in portfolio</li>
-            <li>Start with $100000 cash in the portfolio</li>
-            <li>Stocks must be in the S&P 500</li>
-            <li>All stock must have a weighting between 5-35% of the portfolio</li>
-            <li>Partial shares are allowed</li>
-          </ul>
-        </div>
-        <div class='section'>
-          <SectionTitle>STRATEGY</SectionTitle>
-          <div class='sub-section'>
-            <SectionSubtitle>RISKY</SectionSubtitle>
-            <p>
-              The riskiest stock is the stock with the highest beta stock chosen the 3 highest standard deviations stocks.
-              The other 9 stocks are the most correlated to the riskiest stock.
-            </p>
-            <br></br>
-            <p>
-              When deciding the distribution of cash, 35% is given to the riskiest stock and 5% is automatically given to the other 9.
-              The remaining 20% of weight is distributed to the top 3 most correlated stocks based on the most optimal stand deviation for the entire portfolio.
-            </p>
-            <br></br>
-            <p>
-              This calculation is run once at the beginning of every month.
-              The portfolio is then held till the end of the month when the portfolio is regenerated.
-            </p>
-          </div>
-          <div class='sub-section'>
-            <SectionSubtitle>SAFE</SectionSubtitle>
-            <p>
-              The algorithm behind this portfolio is largely the same as the Risky Portfolio.
-              The only difference is instead of aiming for higher standard deviation, beta, and correlation, it is now aiming for lower standard deviation, beta, and correlation.
-            </p>
-            <br></br>
-            <p>
-              The safeest stock is the stock with the lowest beta stock chosen the 3 lowest standard deviations stocks.
-              The other 9 stocks are the least correlated to the safest stock.
-            </p>
-            <br></br>
-            <p>
-              When deciding the distribution of cash, 10% weight is given to each stock for even diversification.
-            </p>
-            <br></br>
-            <p>
-              This calculation is run once at the beginning of every month.
-              The portfolio is then held till the end of the month when the portfolio is regenerated.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Box type='flex' sx={{ flexDirection: 'column', display: 'flex', alignItems: 'center', backgroundColor: theme.palette.df.darkGreen }}>
+      <Box type='flex' sx={{ flexDirection: 'column', display: 'flex', alignItems: 'center', width: '60vw' }}>
+        
+        <Box sx={{ height: 100 }}></Box>
+        
+        <StockGraph/>
+
+        <Box sx={{ height: '50px' }}></Box>
+
+        <SectionTitle>DEMANDS</SectionTitle>
+
+        <Box sx={{ height: '20px' }}></Box>
+
+        <List>
+            <Typography>1. Must always have 10 Stocks in portfolio</Typography>
+            <Typography>2. Start with $100000 cash in the portfolio</Typography>
+            <Typography>3. Stocks must be in the S&P 500</Typography>
+            <Typography>4. All stock must have a weighting between 5-35% of the portfolio</Typography>
+            <Typography>5. Partial shares are allowed</Typography>
+        </List>
+
+        <SectionTitle>STRATEGY</SectionTitle>
+
+        <Box sx={{ height: '20px' }}></Box>
+
+        <SectionSubtitle>RISKY</SectionSubtitle>
+
+        <Typography>
+          The riskiest stock is the stock with the highest beta stock chosen the 3 highest standard deviations stocks.
+          The other 9 stocks are the most correlated to the riskiest stock.
+          <br></br>
+          <br></br>
+          When deciding the distribution of cash, 35% is given to the riskiest stock and 5% is automatically given to the other 9.
+          The remaining 20% of weight is distributed to the top 3 most correlated stocks based on the most optimal stand deviation for the entire portfolio.
+          <br></br>
+          <br></br>
+          This calculation is run once at the beginning of every month.
+          The portfolio is then held till the end of the month when the portfolio is regenerated.
+        </Typography>
+
+        <Box sx={{ height: '20px' }}></Box>
+
+        <SectionSubtitle>SAFE</SectionSubtitle>
+
+        <Typography>
+          The algorithm behind this portfolio is largely the same as the Risky Portfolio.
+          The only difference is instead of aiming for higher standard deviation, beta, and correlation, it is now aiming for lower standard deviation, beta, and correlation.
+          <br></br>
+          <br></br>
+          The safeest stock is the stock with the lowest beta stock chosen the 3 lowest standard deviations stocks.
+          The other 9 stocks are the least correlated to the safest stock.
+          <br></br>
+          <br></br>
+          When deciding the distribution of cash, 10% weight is given to each stock for even diversification.
+          <br></br>
+          <br></br>
+          This calculation is run once at the beginning of every month.
+          The portfolio is then held till the end of the month when the portfolio is regenerated.
+        </Typography>
+        
+        <Box sx={{ height: 100 }}></Box>
+        
+      </Box>
+    </Box>
   )
 }
 
